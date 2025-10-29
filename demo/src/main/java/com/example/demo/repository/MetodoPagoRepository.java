144d6ccc95c0866d02ba19835691e140a2374730
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2025-10-29 16:49:54 -0500

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/MetodoPagoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.MetodoPago;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MetodoPagoRepository extends JpaRepository<MetodoPago, Integer> {
-}
--- demo/src/main/java/com/example/demo/repository/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Rol;
-
-public interface RolRepository extends JpaRepository<Rol, Integer> {
-    Optional<Rol> findByNombre(String nombre);
-}
