b93d39d3fbd1c9f60e25377a523f28c3a2530ac4
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2025-10-29 23:51:06 -0500

    Update WebConfig.java

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -26,13 +25,4 @@ public class WebConfig implements WebMvcConfigurer {
             }
         };
     }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
-        registry.addResourceHandler("/img/**")
-                .addResourceLocations("classpath:/static/img/");
-    }
 }
-
