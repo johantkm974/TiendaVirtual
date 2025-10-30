cacf185ad8edef5a1267d325ca052294b0b39d25
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2025-10-30 00:18:46 -0500

    Update ProductoRepository.java

--- src/main/java/com/example/demo/repository/ProductoRepository.java
+++ src/main/java/com/example/demo/repository/ProductoRepository.java
@@ -1,13 +1,19 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-    // ✅ Forma correcta para acceder a categoria.id
-    List<Producto> findByCategoria_Id(int id);
+import com.example.demo.model.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto,Integer>{
+     List<Producto> findByCategoriaId(int categoriaId);
+    
+    // 🔥 CONSULTA OPTIMIZADA PARA EVITAR N+1
+    @Query("SELECT p FROM Producto p JOIN FETCH p.categoria")
+    List<Producto> findAllWithCategoria();
+    
+
+
 }
