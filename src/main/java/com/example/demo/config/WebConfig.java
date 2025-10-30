ffd4b5babdc3e67c8033c3fb751b5952d8e6c81b
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2025-10-29 23:07:46 -0500

    Update WebConfig.java

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebGlobalConfig implements WebMvcConfigurer {
 
-    // ✅ Configuración de CORS unificada
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -28,12 +27,10 @@ public class WebGlobalConfig implements WebMvcConfigurer {
         };
     }
 
-    // ✅ Configuración de recursos estáticos e imágenes
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
-
         registry.addResourceHandler("/img/**")
                 .addResourceLocations("classpath:/static/img/");
     }
