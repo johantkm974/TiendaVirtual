aa63e1713c38dfb3220ce157af3215e7e0e157e0
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2025-11-01 14:22:30 -0500

    Update CloudinaryConfig.java

--- src/main/java/com/example/demo/config/CloudinaryConfig.java
+++ src/main/java/com/example/demo/config/CloudinaryConfig.java
@@ -11,9 +11,9 @@ public class CloudinaryConfig {
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", "TU_CLOUD_NAME",
-            "api_key", "TU_API_KEY",
-            "api_secret", "TU_API_SECRET",
+            "cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"),
+            "api_key", System.getenv("CLOUDINARY_API_KEY"),
+            "api_secret", System.getenv("CLOUDINARY_API_SECRET"),
             "secure", true
         ));
     }
